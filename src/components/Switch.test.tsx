216fa11128c84811d37b9463652ad23772b0bf2f
Edwin Kwan <edwinkwantc@gmail.com>
2024-03-29 19:10:38 +0800

    fix: removing jest

---
+++ src/components/Switch.test.tsx
@@ -0,0 +1,20 @@
+// import "@testing-library/jest-dom";
+// import { render, fireEvent } from "@testing-library/react";
+// import Switch from "./Switch";
+
+// describe("Switch", () => {
+//   it("renders correctly", () => {
+//     const { getByRole } = render(<Switch />);
+//     const switchElement = getByRole("checkbox");
+//     expect(switchElement).toBeInTheDocument();
+//   });
+
+//   it("calls onChange when clicked", () => {
+//     const onChange = jest.fn();
+//     const { getByRole } = render(<Switch onChange={onChange} />);
+//     const switchElement = getByRole("checkbox");
+
+//     fireEvent.click(switchElement);
+//     expect(onChange).toHaveBeenCalled();
+//   });
+// });
